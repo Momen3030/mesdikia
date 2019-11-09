f1131a78d9d3fcaf65119401e2afcb71ac45b823
emad-danial <emaddanialrefaat@gmail.com>
2019-11-09 20:30:45 +0200

    Fix

--- app/Http/Controllers/GetCategoryApi.php
+++ app/Http/Controllers/GetCategoryApi.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\cars;
-use App\category;
+//use App\category;
 use Illuminate\Http\Request;
 
 
 class GetCategoryApi extends Controller
 {
     //
-    public function listCars()
-    {
-        $category = category::all();
-        $categoryCount = $category->count();
-        return response()->json(['count of rows'=>$categoryCount,'data'=>$category],200);
-    }
+//    public function listCars()
+//    {
+//        $category = category::all();
+//        $categoryCount = $category->count();
+//        return response()->json(['count of rows'=>$categoryCount,'data'=>$category],200);
+//    }
 }
